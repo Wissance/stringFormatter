e70d7f4afc538c17e8144bcb2d5a0637cd07b5eb
Michael Ushakov <um.nix.user@gmail.com>
2020-08-25 17:13:35 +0500

    more tests, it seems that number formats works fine

--- formatter_test.go
+++ formatter_test.go
@@ -10,4 +10,12 @@ func TestStrFormat(t *testing.T) {
     	                      "Michael Ushakov (Evillord666)", "34", "\"Great Success\"")
     assert.Nil(t, err)
     assert.Equal(t, "Hello i am Michael Ushakov (Evillord666), my age is 34 and i am waiting for \"Great Success\", because i am Michael Ushakov (Evillord666)!", strFormatResult)
+
+    strFormatResult, err = Format("We are wondering if these values would be replaced : {5}, {4}, {0}", "one", "two", "three")
+	assert.Nil(t, err)
+	assert.Equal(t, "We are wondering if these values would be replaced : {5}, {4}, one", strFormatResult)
+
+	strFormatResult, err = Format("No args ... : {0}, {1}, {2}")
+	assert.Nil(t, err)
+	assert.Equal(t, "No args ... : {0}, {1}, {2}", strFormatResult)
 }
