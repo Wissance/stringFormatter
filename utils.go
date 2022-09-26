3ffd19ca3d6eb421fd1bc5609eabb69584fa912f
Michael Ushakov <um.nix.user@gmail.com>
2022-09-26 17:25:17 +0500

    numeric seems to be working

--- utils.go
+++ utils.go
@@ -1,12 +1,14 @@
 package stringFormatter
 
-type numeric interface {
+import "math"
+
+type Numeric interface {
 	int8 | int16 | int32 | int64 | int | uint8 | uint16 | uint32 | uint64 | uint
 }
 
 const maxNumericLength = 20
 
-func numericToStr[T numeric](value T, signed bool, base T) string {
+func NumericToStr[T Numeric](value T, signed bool, base T) string {
 	// we make 20 symbols because uint64 max is 18446744073709551615  = 20 digits
 	actual := value
 	str := make([]byte, maxNumericLength)
@@ -14,7 +16,7 @@ func numericToStr[T numeric](value T, signed bool, base T) string {
 	digits := 0
 	if signed {
 		if value < 0 {
-			actual = value
+			actual = T(math.Abs(float64(actual)))
 			neg = true
 		}
 	}
@@ -22,7 +24,11 @@ func numericToStr[T numeric](value T, signed bool, base T) string {
 	for i, _ := range str {
 		digit := actual % base
 		actual /= base
-		str[maxNumericLength-i-1] = 0x30 + byte(digit)
+		if digit <= 9 {
+			str[maxNumericLength-i-1] = '0' + byte(digit)
+		} else {
+			str[maxNumericLength-i-1] = 'A' + byte(digit) - 10
+		}
 		digits++
 		if actual == 0 {
 			break
@@ -30,7 +36,7 @@ func numericToStr[T numeric](value T, signed bool, base T) string {
 	}
 
 	if neg {
-		str[digits+1] = '-'
+		str[maxNumericLength-digits-1] = '-'
 		digits++
 	}
 
--- utils_test.go
+++ utils_test.go
@@ -6,9 +6,21 @@ import (
 )
 
 func TestNumericToString(t *testing.T) {
-	number := 1234567890
-	result := numericToStr(number, false, 10)
-	assert.Equal(t, "1234567890", result)
+	convertUnsignedAndCheck(t, 9, 10, "9")
+	convertUnsignedAndCheck(t, 1234567890, 10, "1234567890")
+	convertUnsignedAndCheck(t, 0xF1112DCB, 16, "F1112DCB")
+	convertUnsignedAndCheck(t, 0o572643, 8, "572643")
+
+	convertSignedAndCheck(t, -1234, 10, "-1234")
+	convertSignedAndCheck(t, -0xFAC560, 16, "-FAC560")
 }
 
-func convertAndTest[TN numeric](t *testing.T, )
+func convertUnsignedAndCheck[TN Numeric](t *testing.T, number TN, base TN, expectedStr string) {
+	result := NumericToStr(number, false, base)
+	assert.Equal(t, expectedStr, result)
+}
+
+func convertSignedAndCheck[TN Numeric](t *testing.T, number TN, base TN, expectedStr string) {
+	result := NumericToStr(number, true, base)
+	assert.Equal(t, expectedStr, result)
+}
