156281456f61d4412de869b85f57803d5d2b1583
Michael Ushakov <um.nix.user@gmail.com>
2022-10-09 19:56:58 +0500

    one more perf tests

--- formatter_benchmark_test.go
+++ formatter_benchmark_test.go
@@ -6,14 +6,28 @@ import (
 	"time"
 )
 
-func BenchmarkFormat(b *testing.B) {
+func BenchmarkFormat3Arg(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = Format("Today is : {0}, atmosphere pressure is : {1} mmHg, temperature: {2}, location: {3}", time.Now().String(), 725, -1.54, "Yekaterinburg")
 	}
 }
 
-func BenchmarkFmt(b *testing.B) {
+func BenchmarkFmt3Arg(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = fmt.Sprintf("Today is : %s, atmosphere pressure is : %d mmHg, temperature: %f, location: %s", time.Now().String(), 725, -1.54, "Yekaterinburg")
+	}
+}
+
+func BenchmarkFormat5Arg(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Format("Today is : {0}, atmosphere pressure is : {1} mmHg, temperature: {2}, location: {3}, coord:{4}-{5}",
+			time.Now().String(), 725, -1.54, "Yekaterinburg", "64.245", "37.895")
+	}
+}
+
+func BenchmarkFmt5Arg(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = fmt.Sprintf("Today is : %s, atmosphere pressure is : %d mmHg, temperature: %f, location: %s, coords: %s-%s",
+			time.Now().String(), 725, -1.54, "Yekaterinburg", "64.245", "37.895")
 	}
 }
