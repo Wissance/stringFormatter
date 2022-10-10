19f4221b33a2038e5db10e14819a94769f8e805a
Michael Ushakov <um.nix.user@gmail.com>
2022-10-10 09:27:02 +0500

    propagate approach to FormatComplex

--- formatter.go
+++ formatter.go
@@ -109,15 +109,67 @@ func FormatComplex(template string, args map[string]interface{}) string {
 		return template
 	}
 
-	formattedStr := template
-	for key, val := range args {
-		arg := "{" + key + "}"
-		strVal := getItemAsStr(&val)
-		// todo: ignore double curly brackets
-		formattedStr = strings.Replace(formattedStr, arg, strVal, -1)
+	templateLen := len(template)
+	var formattedStr = &strings.Builder{}
+	formattedStr.Grow(templateLen + 22*len(args))
+	j := -1
+	start := strings.Index(template, "{")
+	if start < 0 {
+		return template
 	}
 
-	return formattedStr
+	formattedStr.WriteString(template[:start])
+	for i := start; i < templateLen; i++ {
+
+		if template[i] == '{' {
+			// possibly it is a template placeholder
+			if i == templateLen-1 {
+				break
+			}
+			if template[i+1] == '{' { // todo: umv: this not considering {{0}}
+				formattedStr.WriteByte('{')
+				continue
+			} else {
+				// find end of placeholder
+				j = i + 2
+				for {
+					if j >= templateLen {
+						break
+					}
+					if template[j] == '}' {
+						break
+					}
+					j++
+				}
+				// double curly brackets processed here, convert {{N}} -> {N}
+				// so we catch here {{N}
+				if j+1 < templateLen && template[j+1] == '}' {
+
+					formattedStr.WriteString(template[i+1 : j+1])
+					i = j + 1
+				} else {
+					argNumberStr := template[i+1 : j]
+					arg, ok := args[argNumberStr]
+					if ok {
+						// get number from placeholder
+						strVal := getItemAsStr(&arg)
+						formattedStr.WriteString(strVal)
+					} else {
+						formattedStr.WriteByte('{')
+						formattedStr.WriteString(argNumberStr)
+						formattedStr.WriteByte('}')
+					}
+					i = j
+				}
+			}
+
+		} else {
+			j = i
+			formattedStr.WriteByte(template[i])
+		}
+	}
+
+	return formattedStr.String()
 }
 
 // todo: umv: impl format passing as param
