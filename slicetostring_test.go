6668592f9fb7c6dc86f52873ff402ad5e8810ffc
Ushakov Michale <um.nix.user@gmail.com>
2025-03-18 21:17:11 +0500

    created SliceToString func and test

---
+++ slicetostring_test.go
@@ -0,0 +1,31 @@
+package stringFormatter_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/wissance/stringFormatter"
+	"testing"
+)
+
+func TestSliceToString(t *testing.T) {
+	for name, test := range map[string]struct {
+		separator      string
+		data           []any
+		expectedResult string
+	}{
+		"comma-separated slice": {
+			separator:      ", ",
+			data:           []any{11, 22, 33, 44, 55, 66, 77, 88, 99},
+			expectedResult: "11, 22, 33, 44, 55, 66, 77, 88, 99",
+		},
+		"dash(kebab) line from slice": {
+			separator:      "-",
+			data:           []any{"str1", "str2", 101, "str3"},
+			expectedResult: "str1-str2-101-str3",
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			actualResult := stringFormatter.SliceToString(&test.data, &test.separator)
+			assert.Equal(t, test.expectedResult, actualResult)
+		})
+	}
+}
