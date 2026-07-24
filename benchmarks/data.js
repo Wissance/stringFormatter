b6ae82a4162517018119468ff82bd9d0f24a1629
github-action-benchmark <github@users.noreply.github.com>
2026-07-24 18:59:56 +0000

    add Go Benchmark Results (go) benchmark result for 65bc7d2cc0b197f19a233078726e161611f0d5d2

---
+++ benchmarks/data.js
@@ -0,0 +1,128 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1784919596417,
+  "repoUrl": "https://github.com/Wissance/stringFormatter",
+  "entries": {
+    "Go Benchmark Results": [
+      {
+        "commit": {
+          "author": {
+            "email": "um.nix.user@gmail.com",
+            "name": "Ushakov Michale",
+            "username": "EvilLord666"
+          },
+          "committer": {
+            "email": "um.nix.user@gmail.com",
+            "name": "Ushakov Michale",
+            "username": "EvilLord666"
+          },
+          "distinct": false,
+          "id": "65bc7d2cc0b197f19a233078726e161611f0d5d2",
+          "message": "benchmark run",
+          "timestamp": "2026-07-24T23:58:43+05:00",
+          "tree_id": "78bc001e75d8f228e1fe2eaf7defb40944d5205b",
+          "url": "https://github.com/Wissance/stringFormatter/commit/65bc7d2cc0b197f19a233078726e161611f0d5d2"
+        },
+        "date": 1784919595561,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFormat4Arg",
+            "value": 940.4,
+            "unit": "ns/op",
+            "extra": "1282770 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFormat4ArgAdvanced",
+            "value": 944.5,
+            "unit": "ns/op",
+            "extra": "1273389 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFmt4Arg",
+            "value": 849.9,
+            "unit": "ns/op",
+            "extra": "1408797 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFmt4ArgAdvanced",
+            "value": 943.7,
+            "unit": "ns/op",
+            "extra": "1295596 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFormat6Arg",
+            "value": 1035,
+            "unit": "ns/op",
+            "extra": "1000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFmt6Arg",
+            "value": 905,
+            "unit": "ns/op",
+            "extra": "1330922 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFormatComplex7Arg",
+            "value": 683,
+            "unit": "ns/op",
+            "extra": "1797201 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMapToStringWith11Keys",
+            "value": 2532,
+            "unit": "ns/op",
+            "extra": "440605 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceToStringAdvancedWith8IntItems",
+            "value": 757.1,
+            "unit": "ns/op",
+            "extra": "1591964 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceStandard8IntItems",
+            "value": 882.5,
+            "unit": "ns/op",
+            "extra": "1366578 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceToStringAdvanced10MixedItems",
+            "value": 971.7,
+            "unit": "ns/op",
+            "extra": "1235091 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceToStringAdvanced10TypedItems",
+            "value": 166,
+            "unit": "ns/op",
+            "extra": "7178115 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceStandard10MixedItems",
+            "value": 1318,
+            "unit": "ns/op",
+            "extra": "878785 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceToStringAdvanced20StrItems",
+            "value": 1650,
+            "unit": "ns/op",
+            "extra": "650544 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceToStringAdvanced20TypedStrItems",
+            "value": 1954,
+            "unit": "ns/op",
+            "extra": "567589 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceStandard20StrItems",
+            "value": 2470,
+            "unit": "ns/op",
+            "extra": "480632 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
