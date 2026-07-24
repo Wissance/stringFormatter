2e365b778b045490be745e568d0aaa19d516082f
github-action-benchmark <github@users.noreply.github.com>
2026-07-24 19:04:59 +0000

    add Go Benchmark Results (go) benchmark result for 65bc7d2cc0b197f19a233078726e161611f0d5d2

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1784919596417,
+  "lastUpdate": 1784919899713,
   "repoUrl": "https://github.com/Wissance/stringFormatter",
   "entries": {
     "Go Benchmark Results": [
@@ -120,6 +120,126 @@ window.BENCHMARK_DATA = {
             "value": 2470,
             "unit": "ns/op",
             "extra": "480632 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "um.nix.user@gmail.com",
+            "name": "Ushakov Michale",
+            "username": "EvilLord666"
+          },
+          "committer": {
+            "email": "um.nix.user@gmail.com",
+            "name": "Ushakov Michale",
+            "username": "EvilLord666"
+          },
+          "distinct": false,
+          "id": "65bc7d2cc0b197f19a233078726e161611f0d5d2",
+          "message": "benchmark run",
+          "timestamp": "2026-07-24T23:58:43+05:00",
+          "tree_id": "78bc001e75d8f228e1fe2eaf7defb40944d5205b",
+          "url": "https://github.com/Wissance/stringFormatter/commit/65bc7d2cc0b197f19a233078726e161611f0d5d2"
+        },
+        "date": 1784919899366,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkFormat4Arg",
+            "value": 937.9,
+            "unit": "ns/op",
+            "extra": "1285888 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFormat4ArgAdvanced",
+            "value": 954.5,
+            "unit": "ns/op",
+            "extra": "1273557 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFmt4Arg",
+            "value": 851.2,
+            "unit": "ns/op",
+            "extra": "1415745 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFmt4ArgAdvanced",
+            "value": 921.9,
+            "unit": "ns/op",
+            "extra": "1305236 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFormat6Arg",
+            "value": 1056,
+            "unit": "ns/op",
+            "extra": "982534 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFmt6Arg",
+            "value": 897.6,
+            "unit": "ns/op",
+            "extra": "1333676 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFormatComplex7Arg",
+            "value": 671.6,
+            "unit": "ns/op",
+            "extra": "1793481 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMapToStringWith11Keys",
+            "value": 2564,
+            "unit": "ns/op",
+            "extra": "459105 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceToStringAdvancedWith8IntItems",
+            "value": 754.7,
+            "unit": "ns/op",
+            "extra": "1582873 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceStandard8IntItems",
+            "value": 909.1,
+            "unit": "ns/op",
+            "extra": "1349534 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceToStringAdvanced10MixedItems",
+            "value": 975.4,
+            "unit": "ns/op",
+            "extra": "1222718 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceToStringAdvanced10TypedItems",
+            "value": 165,
+            "unit": "ns/op",
+            "extra": "7195862 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceStandard10MixedItems",
+            "value": 1310,
+            "unit": "ns/op",
+            "extra": "875040 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceToStringAdvanced20StrItems",
+            "value": 1671,
+            "unit": "ns/op",
+            "extra": "646458 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceToStringAdvanced20TypedStrItems",
+            "value": 1951,
+            "unit": "ns/op",
+            "extra": "554414 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkSliceStandard20StrItems",
+            "value": 2496,
+            "unit": "ns/op",
+            "extra": "478153 times\n4 procs"
           }
         ]
       }
